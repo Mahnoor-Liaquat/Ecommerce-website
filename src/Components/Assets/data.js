7796adac3ef6b4895ee2db895034fc7ed3b81e11
muzammilali11 <mahnoorliaquat6@gmail.com>
2024-07-09 23:21:23 +0500

    first commit

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,34 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+
+let data_product = [
+  {
+    id:1,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p1_img,
+    new_price:4000,
+    old_price:6000,
+  },
+  {id:2,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p2_img,
+    new_price:3500,
+    old_price:5000,
+  },
+  {id:3,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p3_img,
+    new_price:2000,
+    old_price:4000,
+  },
+  {id:4,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p4_img,
+    new_price:1000,
+    old_price:1500,
+  },
+];
+
+export default data_product;
---
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './NewsLetter.css'
+
+
+const NewsLetter = () => {
+  return (
+    <div className='newsletter'>
+        <h1>Get Exclusive Offer On Your Email</h1>
+        <p>Subscribe to Our NewsLetter and stay Updated</p>
+    <div>
+        <input type='email' placeholder='your email id'/>
+        <button>Subscribe</button>
+    </div>
+    </div>
+  )
+}
+
+export default NewsLetter
